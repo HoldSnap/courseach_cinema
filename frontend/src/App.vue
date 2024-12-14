13bc57d23a8f1006cdde3975a11f6d0b42381f3b
Stepanov Mikhail <volodya-stepanov-68@gmail.ru>
2024-12-14 15:33:01 +0300

    init: frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+import { defineComponent } from 'vue'
+
+defineComponent({})
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div>тест</div>
+</template>
+
+<style scoped></style>
