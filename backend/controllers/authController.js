c43700baafc3d6a3bffd4df75015a3ce9d7eb736
Stepanov Mikhail <75545388+HoldSnap@users.noreply.github.com>
2024-12-19 20:49:57 +0300

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,37 +2,32 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const {User, Client} =
-    require('../models');  // Импортируем только необходимые модели
+    require('../models');  
 
-// Регистрация нового клиента
 async function register(req, res) {
-  const {login, password, name} = req.body;  // Удалено поле 'role'
+  const {login, password, name} = req.body; 
 
   try {
-    // Проверяем, существует ли уже пользователь с таким логином
     const existingUser = await User.findOne({where: {login}});
     if (existingUser) {
       return res.status(400).json(
           {message: 'Пользователь с таким логином уже существует.'});
     }
 
-    // Хешируем пароль
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Создаем нового пользователя с ролью 'client'
     const user = await User.create({
       login,
       password: hashedPassword,
-      role: 'client',  // Роль устанавливается автоматически
+      role: 'client',  
     });
 
-    // Создаем запись в таблице клиента
     await Client.create({
       userId: user.id,
-      name: name || 'Client',  // Имя клиента можно передать через запрос или
-                               // использовать значение по умолчанию
-      balance: 100.00,  // Начальный баланс клиента, можно задать по умолчанию
-                        // или передать через запрос
+      name: name || 'Client',  
+                               
+      balance: 100.00,  
+                       
     });
 
     return res.status(201).json({message: 'Клиент успешно зарегистрирован!'});
@@ -43,7 +38,6 @@ async function register(req, res) {
   }
 }
 
-// Логин пользователя и получение JWT токена
 async function login(req, res) {
   const {login, password} = req.body;
 
@@ -52,24 +46,21 @@ async function login(req, res) {
   }
 
   try {
-    // Находим пользователя по логину
     const user = await User.findOne({where: {login}});
     if (!user) {
       return res.status(400).json(
           {message: 'Пользователь с таким логином не найден.'});
     }
 
-    // Сравниваем пароли
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(400).json({message: 'Неверный пароль.'});
     }
 
-    // Генерация JWT токена
     const token = jwt.sign(
-        {id: user.id, role: user.role},  // payload (содержимое токена)
-        process.env.JWT_SECRET_KEY,  // секретный ключ из .env
-        {expiresIn: '1h'}            // время жизни токена
+        {id: user.id, role: user.role}, 
+        process.env.JWT_SECRET_KEY, 
+        {expiresIn: '1h'}           
     );
 
     return res.status(200).json({token});
