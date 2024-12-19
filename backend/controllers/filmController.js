7d039c951bdd13465feca9ea17b501bbb4eaa779
Stepanov Mikhail <75545388+HoldSnap@users.noreply.github.com>
2024-12-19 20:50:17 +0300

    Update filmController.js

--- backend/controllers/filmController.js
+++ backend/controllers/filmController.js
@@ -1,6 +1,5 @@
 const {Film} = require('../models');
 
-// Контроллер для создания фильма
 async function createFilm(req, res) {
   try {
     // Проверяем, что объект user существует в запросе
@@ -8,22 +7,18 @@ async function createFilm(req, res) {
       return res.status(401).json({message: 'Пользователь не авторизован.'});
     }
 
-    // Данные фильма, переданные в запросе
     const {title, duration, genre, premiereDate, pointUsageRestriction} =
         req.body;
 
-    // Проверка роли администратора (только администраторы могут создавать
-    // фильмы)
+
     if (req.user.role !== 'administrator') {
       return res.status(403).json(
           {message: 'Только администраторы могут создавать фильмы.'});
     }
 
-    // Создание нового фильма
     const newFilm = await Film.create(
         {title, duration, genre, premiereDate, pointUsageRestriction});
 
-    // Ответ с информацией о созданном фильме
     return res.status(201).json(newFilm);
   } catch (error) {
     console.error(error);
